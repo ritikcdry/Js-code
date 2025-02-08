cea405d571d856da1e09423b9cbff61f12a10475
Ritik Chaudhary Tharu <ritikchaudhary317@gmail.com>
2025-02-08 11:23:44 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -40,8 +40,8 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
-let str1 = "hello"
-let str2 = " ritik "
+let str1 = " Hello "
+let str2 = " Ritik "
 
 let str3 = str1 + str2
 // console.log(str3);
